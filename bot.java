64a011dd3b5ffca3be1c190f55e6f1670884890b
AnshVish14 <supriyatamrakar374@gmail.com>
2023-11-07 11:05:55 +0530

    hmmm

---
+++ bot.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class bot{
+    public static void main(String[] args){
+        Scanner myu = new Scanner(System.in);
+        System.out.println("Aap Ka Swagat Hai '''<|<|Jai_SHREE_RAM|>|>'''");
+        while(true){
+            String command = myu.nextLine();
+            if(command.equals("namaskar")){
+                System.out.println("swagat hai");
+            }
+            else if(command.equals("how are your sir ")){
+                System.out.println("how are you");
+            }
+            else{
+                System.out.println("bye");
+            }
+        }
+    }
+}
